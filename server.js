04b6af78848e91bd67dde476a9ee70547ceb798e
Farrah-solve <68975737+Farrah-solve@users.noreply.github.com>
2021-02-03 14:17:02 -0500

    creating server connection

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+app.use(cors());
+app.use(express.json());
+
+
+//connect to mongoose
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+
+// require route
+
+app.listen(3001, function() {
+    console.log("express server is running on port 3001");
+})
+
