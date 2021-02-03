1d3a8db0c0ba895b8625b585b161850a1196c97c
Farrah-solve <68975737+Farrah-solve@users.noreply.github.com>
2021-02-03 11:03:35 -0500

    navbar categories

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 function Navbar() {
     return <nav className='navbar bg-dark container'>
-        
+        <h4>Home</h4>
+        <h4>Budget</h4>
+        <h4>Notes</h4>
     </nav>
 }
 
